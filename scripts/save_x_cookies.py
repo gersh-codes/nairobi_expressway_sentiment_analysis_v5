b7afc1fde27738e148a9837183e57ab64ec453ce
gersh-codes <gershomochanda@gmail.com>
2025-07-03 21:59:41 +0300

    Torch DLL Error Fix

--- scripts/save_x_cookies.py
+++ scripts/save_x_cookies.py
@@ -5,6 +5,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+print(">>> REQUESTS_CA_BUNDLE =", repr(os.environ.get("REQUESTS_CA_BUNDLE")))
+print(">>> SSL_CERT_FILE      =", repr(os.environ.get("SSL_CERT_FILE")))
+
 os.makedirs("cookies", exist_ok=True)
 
 options = Options()
